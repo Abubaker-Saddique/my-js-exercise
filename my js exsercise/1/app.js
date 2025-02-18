bdf9beb7c04a3795d217ede6cb6c2653ac292bd6
Abubaker-Saddique <bakkarmian@gmail.com>
2025-02-18 17:16:40 +0500

    Add files via upload

---
+++ my js exsercise/1/app.js
@@ -0,0 +1,6 @@
+alert("welcome brother");
+alert("error please Enter a valid password");
+alert("welcome to js land... \n Happy coding!");
+alert("welcome to js Land");
+alert("Happy coding !");
+alert("hello .. i can run js through my web browsers concoles");
